63bd3db280ac3047330a8a7da4d6263520c619f4
nanimono <nan1mono@163.com>
2024-02-02 11:20:15 +0800

    pref：简化代码

--- src/main/java/com/project/template/logger/core/filter/LogMongoMvcFilter.java
+++ src/main/java/com/project/template/logger/core/filter/LogMongoMvcFilter.java
@@ -17,17 +17,12 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -51,15 +46,15 @@ public class LogMongoMvcFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
         // 避免重复初始化
-        if (StringUtils.isBlank(enableProperty)){
+        if (StringUtils.isBlank(enableProperty)) {
             ServletContext sc = filterConfig.getServletContext();
             WebApplicationContext cxt = WebApplicationContextUtils.getWebApplicationContext(sc);
-            if (cxt == null){
+            if (cxt == null) {
                 return;
             }
             enableProperty = cxt.getEnvironment().getProperty("template.logger.mongo.enable");
             String excludedStr = cxt.getEnvironment().getProperty("template.logger.excluded");
-            if (StringUtils.isNotBlank(excludedStr)){
+            if (StringUtils.isNotBlank(excludedStr)) {
                 try {
                     excluded = List.of(excludedStr.split(","));
                 } catch (Exception e) {
@@ -67,14 +62,14 @@ public class LogMongoMvcFilter implements Filter {
                     excluded = Collections.emptyList();
                 }
             }
-            if (StringUtils.isBlank(bodyProperty)){
+            if (StringUtils.isBlank(bodyProperty)) {
                 bodyProperty = cxt.getEnvironment().getProperty("template.logger.mongo.body");
             }
         }
-        if (StringUtils.isNotBlank(enableProperty)){
+        if (StringUtils.isNotBlank(enableProperty)) {
             this.enable = Boolean.parseBoolean(enableProperty);
         }
-        if (StringUtils.isNotBlank(bodyProperty)){
+        if (StringUtils.isNotBlank(bodyProperty)) {
             this.body = Boolean.parseBoolean(bodyProperty);
         }
         WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(filterConfig.getServletContext());
@@ -93,7 +88,6 @@ public class LogMongoMvcFilter implements Filter {
         }
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        ContentCachingRequestWrapper contentCachingRequestWrapper = new ContentCachingRequestWrapper(httpRequest);
         ContentCachingResponseWrapper contentCachingResponseWrapper = new ContentCachingResponseWrapper(httpResponse);
         String requestURI = httpRequest.getRequestURI();
         // 跳过所有js，css和ico资源
@@ -106,45 +100,7 @@ public class LogMongoMvcFilter implements Filter {
             chain.doFilter(request, response);
             return;
         }
-        long startTime = System.currentTimeMillis();
-        chain.doFilter(contentCachingRequestWrapper, contentCachingResponseWrapper);
-        long endTime = System.currentTimeMillis();
-        // 创建日志对象
-        TemplateLog templateLog = new TemplateLog();
-        templateLog.setRequestIP(request.getRemoteAddr())
-                .setRequestURL(requestURI)
-                .setRequestTime(LocalDateTime.now())
-                .setMethod(httpRequest.getMethod())
-                .setContentType(request.getContentType())
-                .setHttpStatus(httpResponse.getStatus())
-                .setDuration(endTime - startTime);
-        // 设置请求体
-        if (body) {
-            // 读取POST请求体
-            String requestBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-            // 读取GET请求体
-            StringBuilder enumeration = new StringBuilder();
-            Enumeration<String> parameterNames = request.getParameterNames();
-            while (parameterNames.hasMoreElements()) {
-                String paramName = parameterNames.nextElement();
-                String paramValue = request.getParameter(paramName);
-                if (parameterNames.hasMoreElements()) {
-                    enumeration.append(String.format("%s=%s&", paramName, paramValue));
-                } else {
-                    enumeration.append(String.format("%s=%s", paramName, paramValue));
-                }
-            }
-            templateLog.setEnumeration(enumeration.toString());
-            templateLog.setRequestBody(requestBody);
-            // 获取响应体
-            // 输出响应请求，如果请求响应是一个非文本，则跳过
-            String contentType = contentCachingResponseWrapper.getContentType();
-            if (contentType != null && (contentType.startsWith("text/") || contentType.startsWith("application/json"))) {
-                byte[] contentAsByteArray = contentCachingResponseWrapper.getContentAsByteArray();
-                String responseBody = new String(contentAsByteArray, StandardCharsets.UTF_8);
-                templateLog.setResponseBody(responseBody);
-            }
-        }
+        TemplateLog templateLog = TemplateLog.packageTemplate(request, response, chain, body);
         // 保存日志
         mongoTemplate.save(templateLog);
         // 回写响应
